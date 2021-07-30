5441c6951e6279291adcc01422e720109ceb5b5e
Andre Madarang <andre_madarang@hotmail.com>
2021-07-30 14:20:36 -0400

    Optimize transitions

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const menuItems = document.querySelectorAll('nav ul li')
 const menuBackground = document.querySelector('.menu-background')
+const nav = document.querySelector('nav ul')
+const arrow = document.querySelector('.arrow')
 
 menuItems.forEach(menuItem => menuItem.addEventListener('mouseenter', handleEnter))
 menuItems.forEach(menuItem => menuItem.addEventListener('mouseleave', handleLeave))
@@ -11,13 +13,30 @@ function handleEnter() {
   setTimeout(() => menu.classList.add('menu-enter-active'), 50)
 
   const menuCoords = menu.getBoundingClientRect()
+  const navCoords = nav.getBoundingClientRect()
 
   menuBackground.classList.add('open')
+  arrow.classList.add('open')
 
-  menuBackground.style.setProperty('width', `${menuCoords.width}px`)
-  menuBackground.style.setProperty('height', `${menuCoords.height}px`)
-  menuBackground.style.setProperty('top', `${menuCoords.top + window.scrollY}px`)
-  menuBackground.style.setProperty('left', `${menuCoords.left}px`)
+  // menuBackground.style.setProperty('width', `${menuCoords.width}px`)
+  // menuBackground.style.setProperty('height', `${menuCoords.height}px`)
+  // menuBackground.style.setProperty('top', `${menuCoords.top + window.scrollY}px`)
+  // menuBackground.style.setProperty('left', `${menuCoords.left}px`)
+
+  menuBackground.style.setProperty('transform',
+    `
+      translate(${menuCoords.left}px, ${menuCoords.top + window.scrollY}px)
+      scaleX(${menuCoords.width/100})
+      scaleY(${menuCoords.height/100})
+    `
+  )
+
+  arrow.style.setProperty('transform',
+    `
+      translate(${menuCoords.left + (menuCoords.width / 2) - 7}px, ${menuCoords.top - navCoords.top}px)
+      rotate(45deg) translateY(-50%)
+    `
+  )
 }
 
 function handleLeave() {
@@ -25,6 +44,7 @@ function handleLeave() {
 
   menu.classList.remove('menu-enter', 'menu-enter-active')
   menuBackground.classList.remove('open')
+  arrow.classList.remove('open')
 
   // menu.classList.add('menu-leave')
   // setTimeout(() => menu.classList.add('menu-leave-active'), 50)
