0c3b70221cdef96c83cbf4f011a873aa9c1c6221
Andre Madarang <andre_madarang@hotmail.com>
2021-07-30 13:44:10 -0400

    Width and height transitions working

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const menuItems = document.querySelectorAll('nav ul li')
+const menuBackground = document.querySelector('.menu-background')
 
 menuItems.forEach(menuItem => menuItem.addEventListener('mouseenter', handleEnter))
 menuItems.forEach(menuItem => menuItem.addEventListener('mouseleave', handleLeave))
@@ -8,12 +9,22 @@ function handleEnter() {
 
   menu.classList.add('menu-enter')
   setTimeout(() => menu.classList.add('menu-enter-active'), 50)
+
+  const menuCoords = menu.getBoundingClientRect()
+
+  menuBackground.classList.add('open')
+
+  menuBackground.style.setProperty('width', `${menuCoords.width}px`)
+  menuBackground.style.setProperty('height', `${menuCoords.height}px`)
+  menuBackground.style.setProperty('top', `${menuCoords.top + window.scrollY}px`)
+  menuBackground.style.setProperty('left', `${menuCoords.left}px`)
 }
 
 function handleLeave() {
   const menu = this.querySelector('.menu')
 
   menu.classList.remove('menu-enter', 'menu-enter-active')
+  menuBackground.classList.remove('open')
 
   // menu.classList.add('menu-leave')
   // setTimeout(() => menu.classList.add('menu-leave-active'), 50)
